717c789640971ac1eba4c3f2ac86b10800c2e245
anton <an.horunzhij@dodopizza.com>
2022-04-23 08:58:17 +0300

    small fixes

--- features/weather/src/main/java/WeatherViewModel.kt
+++ features/weather/src/main/java/WeatherViewModel.kt
@@ -1,11 +1,9 @@
 package com.petproject.weatherapp.weather
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.petproject.weatherapp.cities.domain.WeatherUseCase
 import com.petproject.weatherapp.cities.domain.model.WeatherParams
-import com.petproject.weatherapp.weather.WeatherUIModule
 import com.petproject.weatherapp.common.flowbinding.WhileViewSubscribed
 import com.petproject.weatherapp.weather.WeatherUIModel.Content
 import com.petproject.weatherapp.weather.WeatherUIModel.Loading
@@ -13,12 +11,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -36,20 +31,20 @@ class WeatherViewModel @Inject constructor(
     emitAll(refreshSignal)
   }
 
-  private val pull = MutableSharedFlow<Boolean>()
+  private val pullToRefreshSignal = MutableSharedFlow<Boolean>()
 
-  private val pullSignal: Flow<Boolean> = flow {
-    emitAll(pull)
+  private val pullDataSignal: Flow<Boolean> = flow {
+    emitAll(pullToRefreshSignal)
   }
 
   val uiState: StateFlow<WeatherUIModel> = loadDataSignal.mapLatest {
     Content(city = args.name, country = args.country, weatherUseCase(WeatherParams(args.lat, args.lon)))
-  }.onEach { pull.emit(false) }.stateIn(viewModelScope, WhileViewSubscribed, Loading)
+  }.onEach { pullToRefreshSignal.emit(false) }.stateIn(viewModelScope, WhileViewSubscribed, Loading)
 
-  val isRefreshing: StateFlow<Boolean> = pullSignal.stateIn(viewModelScope, WhileViewSubscribed, false)
+  val isRefreshing: StateFlow<Boolean> = pullDataSignal.stateIn(viewModelScope, WhileViewSubscribed, false)
 
   fun refresh() {
+    viewModelScope.launch { pullToRefreshSignal.emit(true) }
     viewModelScope.launch { refreshSignal.emit(Unit) }
-    viewModelScope.launch { pull.emit(true) }
   }
 }
